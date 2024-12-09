a38cbd33a8159a919b156a68e1166a1b71f25e8a
tapo4eqq <tapoekkiselev@gmail.com>
2024-12-09 13:12:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById('cta-button').addEventListener('click', function() {
+    alert('Спасибо за ваш интерес! Мы скоро свяжемся с вами.');
+});
+document.getElementById('cta-button2').addEventListener('click', function() {
+    alert('Спасибо за ваш интерес! Мы скоро свяжемся с вами.');
+});
